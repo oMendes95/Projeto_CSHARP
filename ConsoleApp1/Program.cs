81d3f8a1428f47d0d9c878136a9989f00ec84ecd
pdrlcx <mendesv460@gmail.com>
2022-07-12 17:16:45 -0300

    Projeto Csharp concluido

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OlaMundo1 {
+    class HelloWorld{
+        static void Main(string[] args) {
+            /*Meu Primeiro Projeto Csharp*/
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
